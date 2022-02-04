134217093a824d1e7ea4b29438d2ec3a04d6234c
william <wille.vincent@hotmail.com>
2022-02-04 10:52:59 +0100

    Update lddw.h

--- openGLproj/src/lddw.h
+++ openGLproj/src/lddw.h
@@ -1,12 +1,9 @@
 #pragma once
 
- // For use by lddw applications.
-
+// For use by lddw applications.
 #include "lddw/Application.h"
 #include "lddw/Log.h"
 
 // ----Entry point---------------d
-// 
 #include "lddw/EntryPoint.h"
-// 
 // ------------------------------d
